18c447a7ffc34a6c1fffbd9db5586658ccd2d373
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-10-03 23:20:40 -0500

    Service Worker Activate

--- sw.js
+++ sw.js
@@ -3,5 +3,21 @@
 
 self.addEventListener('install', event => {
 
+    //Descargar Assets
+    //creamos cache
     console.log('Instalando SW');
+
+    //self.skipWaiting();
+
+});
+
+
+//Cuando el SW toma el control de la aplicación
+self.addEventListener('activate', event => {
+
+    //Borrar cache viejo
+
+    console.log('SW2: Activo y listo para controlar app');
+
+
 });
